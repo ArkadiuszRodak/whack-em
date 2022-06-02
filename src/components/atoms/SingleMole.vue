00b4bcfe72696ee8af3f025f425e16bd8c8febc9
Arkadiusz Rodak <arkadiusz.rodak.89@gmail.com>
2022-06-02 18:33:08 +0200

    build: tailwind and some configs

---
+++ src/components/atoms/SingleMole.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    SingleMole
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SingleMole',
+});
+</script>
---
+++ src/components/organisms/MolesGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid grid-cols-3">
+    <div v-for="y in Array(calculatedGrid.ys).keys()" :key="`y${y}`">
+      <div v-for="x in Array(calculatedGrid.xs).keys()" :key="`x${x}`">
+        <span v-text="`${y} ${x}`" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MolesGrid',
+  setup() {
+    const calculateGrid = (): { xs: number; ys: number } => ({
+      xs: 3,
+      ys: 3,
+    });
+    const calculatedGrid = calculateGrid();
+
+    return { calculatedGrid };
+  },
+});
+</script>
